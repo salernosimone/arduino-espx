db0460292a266ac73a31b4f0eccab805b474d5b8
Simone Salerno <info@salernosimone.com>
2025-08-04 14:58:58 +0200

    jsonx

---
+++ examples/jsonx/StreamJsonString/StreamJsonString.ino
@@ -0,0 +1,41 @@
+#include <espx.h>
+#include <espx/jsonx.h>
+
+
+// stream JSON to Serial
+Jsonx jsonx(Serial);
+
+
+void setup() {
+  delay(1000);
+  Serial.begin(115200);
+  Serial.println("JSONx example: print to Serial");
+}
+
+
+void loop() {
+  /**
+   * Create a JSON object with scalar and
+   * composite values
+   */
+  jsonx.rootObject({
+    jsonx.scalar("name", "John"),
+    jsonx.scalar("age", 20),
+    jsonx.object("address", {
+      jsonx.scalar("city", "Rome"),
+      jsonx.scalar("country", "Italy")
+    }),
+    jsonx.list("scores", 10, 20, 30),
+    jsonx.list(
+      "children",
+      jsonx.object(),
+      jsonx.null(),
+      jsonx.object({
+        jsonx.scalar("name", "Jane"),
+        jsonx.list("hobbies", "Ping pong", "Boxing")
+      })
+    )
+  });
+
+  delay(2000);
+}
